d22708f25f4224aff1eefbbdf26f061c43bbf244
Fan Jin <fanjin1989@gmail.com>
2015-11-30 01:01:48 -0800

    fetch condition complete

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ var cheerio = require('cheerio');
 
 app.get('/', (req, res) => {
 
-  var crawler = new Crawler('http://www.wklken.me/');
-  crawler.interval = 500;
-  crawler.maxDepth = 1;
+  var count = 0;
+  var crawler = new Crawler('http://www.vanpeople.com/c/');
+  crawler.interval = 1;
+  crawler.maxDepth = 0;
+  crawler.maxListenerCurrency = 100;
+
+
+  crawler.addFetchCondition((purl) => {
+    return !!purl.path.match(/\/c\/[0-9]*\.html/);
+  });
 
   crawler.on('start', () => {
     console.log('Start crawling');
+  });
+
+
+  crawler.on("fetcherror", (a, b) => {
+    console.log(b.statusCode);
   });
 
   crawler.on('timeout', () => {
@@ -19,7 +31,7 @@ app.get('/', (req, res) => {
   });
 
   crawler.on('fetchstart', (queueItem) => {
-    console.log('start fetching', queueItem.url);
+    // console.log('start fetching', queueItem.url);
   });
 
   crawler.on('fetchredirect', (queueItem, targetl) => {
@@ -27,15 +39,17 @@ app.get('/', (req, res) => {
   });
 
   crawler.on('fetchcomplete', (queueItem, buffer) => {
-    var html = buffer.toString();
-    var $ = cheerio.load(html);
-    $('.show a strong').each(function(i, element){
-      console.log($(element).text());
-    });
+    // var html = buffer.toString();
+    // var $ = cheerio.load(html);
+    // $('.show a strong').each(function(i, element){
+      // console.log($(element).text());
+    // });
+    count++;
     console.log('finished fetching', queueItem.url);
   });
 
   crawler.on('complete', () => {
+    console.log(count);
     console.log('Finished crawling');
   });
 
