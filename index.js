2aa55112d9eaac7f23bfe37408d2032699ec2882
Fan Jin <fanjin1989@gmail.com>
2015-11-29 19:16:11 -0800

    functional finished

--- index.js
+++ index.js
@@ -2,22 +2,43 @@
 var express = require('express');
 var app     = express();
 var Crawler = require('./lib/crawler');
+var cheerio = require('cheerio');
 
 app.get('/', (req, res) => {
 
-  var crawler = new Crawler('http://www.cgiffard.com/');
+  var crawler = new Crawler('http://www.wklken.me/');
+  crawler.interval = 500;
+  crawler.maxDepth = 1;
 
   crawler.on('start', () => {
-    console.log('start crawling');
+    console.log('Start crawling');
   });
 
   crawler.on('timeout', () => {
     console.log('timeout');
   });
 
-  crawler.on('complete', () => {
-    console.log('finished crawling');
+  crawler.on('fetchstart', (queueItem) => {
+    console.log('start fetching', queueItem.url);
   });
+
+  crawler.on('fetchredirect', (queueItem, targetl) => {
+    console.log('REDIRECTED', queueItem.url, 'to', targetl);
+  });
+
+  crawler.on('fetchcomplete', (queueItem, buffer) => {
+    var html = buffer.toString();
+    var $ = cheerio.load(html);
+    $('.show a strong').each(function(i, element){
+      console.log($(element).text());
+    });
+    console.log('finished fetching', queueItem.url);
+  });
+
+  crawler.on('complete', () => {
+    console.log('Finished crawling');
+  });
+
 
   crawler.start();
 
